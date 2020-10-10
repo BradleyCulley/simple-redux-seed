1bc4260f9fdaef1f89e0a926729ad55319b507e4
Brad <culleyib@gmail.com>
2020-10-09 23:42:41 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Instructions:
+-
+1. Run `npm install`
+2. Run `npm start`
+3. Navigate to `localhost:3000` in your browser
+4. Click the "Increment" button to see the count change.
+
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {createStore} from "redux";
+import {incrementReducer} from "./reducers/incrementReducer";
+import {Provider} from "react-redux";
+import {CounterContainer} from "./components/Counter";
+
+const store = createStore(incrementReducer);
+
+render(
+    <Provider store={store}>
+        <CounterContainer/>
+    </Provider>,
+  document.getElementById('root')
+);
